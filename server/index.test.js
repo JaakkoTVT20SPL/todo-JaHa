43b54da1a29fec525040d2105c08fb204b40edf6
JaakkoTVT20SPO <79057530+JaakkoTVT20SPL@users.noreply.github.com>
2024-11-11 21:19:38 +0200

    Rikkinäinen koodi

--- server/index.test.js
+++ server/index.test.js
@@ -1,8 +1,14 @@
+import { initializeTestDb } from "./helpers/test"
+
+var base_url = 'http://localhost:3001/'
+
 import { expect } from "chai"
 
-const base_url = 'http://localhost:3001/'
-
 describe('GET Tasks',() => {
+    before(() => {
+        initializeTestDb()
+    })
+
     it ('should get all tasks',async() => {
         const response = await fetch('http://localhost:3001/')
         const data = await response.json()
@@ -37,12 +43,12 @@ describe('DELETE Task',() => {
 
 describe('POST Task',() => {
     it ('should post a task',async() => {
-        const response = await fetch(base_url + '/create',{
+        const response = await fetch(base_url + 'create',{
             method: 'post',
             headers: {
-                'Content-type':'application/json'
+                'Content-Type':'application/json'
             },
-            body: JSON.stringify({'description':'Task form unit test'})
+            body: JSON.stringify({'description':'Task from unit test'})
         })
         const data = await response.json()
         expect(response.status).to.equal(200)
@@ -51,10 +57,10 @@ describe('POST Task',() => {
     })
 
     it ('should not post a task without description',async () => {
-        const response = await fetch(base_url + '/create',{
+        const response = await fetch(base_url + 'create',{
             method: 'post',
             headers: {
-                'Content-type':'application/json'
+                'Content-Type':'application/json'
             },
             body: JSON.stringify({'description':null})
         })
@@ -62,5 +68,42 @@ describe('POST Task',() => {
         expect(response.status).to.equal(500)
         expect(data).to.be.an('object')
         expect(data).to.include.all.keys('error')
+    })
+})
+
+describe('POST register',() => {
+    const email = 'register@foo.com'
+    const password = 'register123'
+    it ('should register with valid email and password',async() => {
+        const response = await fetch(base_url + 'user/register',{
+            method: 'post',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({'email':email,'password':password})
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(201,data.error)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('id','email')
+    })
+})
+
+describe('POST login',() => {
+    const email = 'login@foo.com'
+    const password = 'login123'
+    insertTestUser(email,password)
+    it ('should login with valid credentials',async() => {
+        const response = await fetch(base_url + '/user/login',{
+            method: 'post',
+            headers: {
+                'Content-Type':'application/json'
+            },
+            body: JSON.stringify({'email':email,'password':password})
+        })
+        const data = await response.json()
+        expect(response.status).to.equal(200,data.error)
+        expect(data).to.be.an('object')
+        expect(data).to.include.all.keys('id','email','token')
     })
 })
